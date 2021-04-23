3e4a4d47b305896365c79ee12f1f68a7a0aa4d71
imsk17 <imsumitkumar17@gmail.com>
2021-04-23 15:40:26 +0530

    :package: Add Some Necessary Packages to the Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,34 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
 
+    //For SMS and MMS purposes
+    implementation ("com.klinkerapps:android-smsmms:5.2.6")
+
+    // JSON Moment
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+
+    // AndroidX KTX
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation ("androidx.work:work-runtime-ktx:2.6.0-alpha02")
+
+    // Apache - Commons
+    implementation ("commons-io:commons-io:2.7")
+    implementation ("org.apache.commons:commons-collections4:4.4")
+    implementation ("org.apache.commons:commons-lang3:3.11")
+
+    // SSHD - For SSH and Stuff
+    implementation ("org.apache.sshd:sshd-core:0.14.0")
+    implementation ("org.apache.mina:mina-core:2.0.19")
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+
+    // Some Necessary Packages
+    implementation ("androidx.collection:collection-ktx:1.1.0")
+    implementation ("com.jaredrummler:android-device-names:1.1.9")
+    implementation ("com.madgag.spongycastle:bcpkix-jdk15on:1.58.0.0")
 }
